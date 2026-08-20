07ea148f9169ade8447ce4516bf80696a0aac490
Arpit Ora <ArpitOra6264@Gmail.com>
2026-08-20 18:55:07 +0530

    Name change

--- Start.py
+++
@@ -1,5 +0,0 @@
-# In This Python script, we will learn how to output data using print() function.
-print("Hello World!")
-print("Welcome to the Python script!")
-print("This script demonstrates how to output data using the print() function.")
-print("By :- ARPIT ORA") 
--- project 02.py
+++
@@ -1,5 +0,0 @@
-#In This Python Script We Will Take Output From Variable And Display It Using Print() Function.
-Name = "Arpit Ora"
-Age = 20
-print("Name:", Name)
-print("Age:", Age)
