1d2787109e15f4fb10845a346c607a8bc7e805ef
Arpit Ora <ArpitOra6264@Gmail.com>
2026-08-20 19:08:28 +0530

    Taking Input And Give Output
    In This Python Script We Will Take Input From User And Display It Using Print() Function.

---
+++ Project03.py
@@ -0,0 +1,7 @@
+#In This Python Script We Will Take Input From User And Display It Using Print() Function.
+
+Name = input("Enter your Name: ")
+
+print("I Am Taking Your Name As Input...")
+
+print("Hello, " + Name + "! Welcome to the Python world.")
