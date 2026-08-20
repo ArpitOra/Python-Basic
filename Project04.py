d392967f6a80697c86c4337a5b1a70a5fceb57b3
Arpit Ora <ArpitOra6264@Gmail.com>
2026-08-20 19:12:50 +0530

    + , - , * , / Of Two Numbers With Use Of Input Command
    In this Script We Will Take Two Numbers From User And Display Their Sum , Subtraction , Product And Division.

---
+++ Project04.py
@@ -0,0 +1,15 @@
+#In This Script We Will Take Two Numbers From User And Display Their Sum,Subtraction,Product And Division.
+
+N1=int(input("Enter First Number: "))
+
+N2=int(input("Enter Second Number: "))
+
+print("I Am Calculating The Sum,Subtraction,Product And Division Of Two Numbers...")
+
+print("Please Wait...")
+
+print("And The Result Is...")
+
+print("Sum of Two Numbers is: ", N1+N2, "\nSubstraction of two number is : " ,N1-N2, "\n Product of two number is : ", N1*N2, "\n Division of two number is : ", N1/N2)
+
+print("Thank You For Using This Python Script. Have A Great Day!")
